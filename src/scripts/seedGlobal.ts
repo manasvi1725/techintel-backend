6787e5fbcc11162bbe79d326954e3fc79cc70de1
Khyati Singh <142620584+human-in-tech@users.noreply.github.com>
2026-01-26 22:19:28 +0000

    reverted back to the codebase before DB changes

--- src/scripts/seedGlobal.ts
+++ src/scripts/seedGlobal.ts
@@ -1,44 +1,44 @@
-import "dotenv/config"
-import fs from "fs"
-import path from "path"
-import  {connectDB}  from "../lib/mongodb.ts"
-import  Technology  from "../models/technology.ts"
+// import "dotenv/config"
+// import fs from "fs"
+// import path from "path"
+// import  {connectDB}  from "../lib/mongodb.ts"
+// import  Technology  from "../models/technology.ts"
 
-async function seedGlobal() {
-  try {
-    await connectDB()
+// async function seedGlobal() {
+//   try {
+//     await connectDB()
 
-    const basePath = path.resolve("data") // adjust if needed
+//     const basePath = path.resolve("data") // adjust if needed
 
-    const patents = JSON.parse(
-      fs.readFileSync(path.join(basePath, "india/patents.json"), "utf-8")
-    )
+//     const patents = JSON.parse(
+//       fs.readFileSync(path.join(basePath, "india/patents.json"), "utf-8")
+//     )
 
-    const publications = JSON.parse(
-      fs.readFileSync(path.join(basePath, "india/publications.json"), "utf-8")
-    )
+//     const publications = JSON.parse(
+//       fs.readFileSync(path.join(basePath, "india/publications.json"), "utf-8")
+//     )
 
-    await Technology.findOneAndUpdate(
-      { name: "__india__" },
-      {
-        name: "__india__",
-        latest_json: {
-          india: {
-            publications,
-            patents          
-          },
-        },
-        updated_at: new Date(),
-      },
-      { upsert: true, new: true }
-    )
+//     await Technology.findOneAndUpdate(
+//       { name: "__india__" },
+//       {
+//         name: "__india__",
+//         latest_json: {
+//           india: {
+//             publications,
+//             patents          
+//           },
+//         },
+//         updated_at: new Date(),
+//       },
+//       { upsert: true, new: true }
+//     )
 
-    console.log("✅ Indian data seeded successfully")
-    process.exit(0)
-  } catch (err) {
-    console.error("❌ Error seeding indian data:", err)
-    process.exit(1)
-  }
-}
+//     console.log("✅ Indian data seeded successfully")
+//     process.exit(0)
+//   } catch (err) {
+//     console.error("❌ Error seeding indian data:", err)
+//     process.exit(1)
+//   }
+// }
 
-seedGlobal()
+// seedGlobal()
